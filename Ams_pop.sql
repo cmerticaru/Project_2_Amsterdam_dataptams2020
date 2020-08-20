4eb15d91c57d8b221fcdb9613f8119871bead37c
Mugur Merticaru <codrutza.lupea@gmail.com>
2020-08-20 20:46:13 +0200

    last database structure

--- Ams_pop.sql
+++ Ams_pop.sql
@@ -6,32 +6,27 @@ CREATE TABLE Population (
     Neighborhood_name VARCHAR(45),
     Population_male INT,
     Population_female INT,
-    idHouseholds int NOT NULL UNIQUE,
-    idFelonies int NOT NULL UNIQUE,
     PRIMARY KEY (idPopulation)
-);
 CREATE TABLE Households (
     idHouseholds int NOT NULL UNIQUE AUTO_INCREMENT,
+    Total VARCHAR(45),
     Neighborhood_code VARCHAR(45),
     Neighborhood_name VARCHAR(45),
     Households_except_couple_with_kids INT,
     Couples_with_children INT,
-    idPopulation int NOT NULL UNIQUE,
-    idFelonies int NOT NULL UNIQUE,
     PRIMARY KEY (idHouseholds)
 );
-
 CREATE TABLE Felonies (
     idFelonies int NOT NULL UNIQUE AUTO_INCREMENT ,
     Neighborhood_code VARCHAR(45),
     Neighborhood_name VARCHAR(45),
+    Total_population INT,
     Sum_felonies INT,
+    Counterfeiting INT,
     Crimes_against_property INT,
     Theft INT,
     Vandalism_agression_against_law_enforcement INT,
     Violence_SexualAssault INT,
-    idPopulation int NOT NULL UNIQUE,
-    idHouseholds int NOT NULL UNIQUE,
     PRIMARY KEY (idFelonies)
 );
 
